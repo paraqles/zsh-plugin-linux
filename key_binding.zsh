f1a997e468aa2e521a147ec260c60038c677cf30
paraqles <paraqles@dthal.net>
2014-12-15 12:27:49 +0100

    Changed keybinding to hash structure.

--- key_binding.zsh
+++ key_binding.zsh
@@ -1,22 +1,12 @@
-bindkey '[1~'     beginning-of-line
-bindkey '[3~'     delete-char
-bindkey '[4~'     end-of-line
-bindkey '[5~'     up-line-or-history
-bindkey '[6~'     down-line-or-history
+bindkey "${key[home]}"         beginning-of-line
+bindkey "${key[end]}"          end-of-line
 
-bindkey '[1;5A'   up-line-or-history
-bindkey '[1;5B'   down-line-or-history
-bindkey '[1;5C'   forward-word
-bindkey '[1;5D'   backward-word
+bindkey "${key[delete]}"       delete-char
+bindkey "${key[backspace]}"    backward-delete-char
 
-bindkey '^?'        backward-delete-char
+bindkey "${key[up]}"           up-line-or-history
+bindkey "${key[down]}"         down-line-or-history
 
-if [[ ! -z $TMUX ]]; then
-  source $ZSH_PLUGIN_LINUX_DIR/key_binding.tmux.zsh
-elif [[ $COLORTERM = "gnome-terminal" ]]; then
-  source $ZSH_PLUGIN_LINUX_DIR/key_binding.gnome-terminal.zsh
-else
-  source $ZSH_PLUGIN_LINUX_DIR/key_binding.shell.zsh
-fi
-
+bindkey "${key[ctrl-right]}"   forward-word
+bindkey "${key[ctrl-left]}"    backward-word
 
