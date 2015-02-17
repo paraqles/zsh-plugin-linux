c4bb57b92fc38e82195f9ab77eb5211a9dca4508
paraqles <paraqles@dthal.net>
2015-02-17 17:06:58 +0100

    changed key array

--- key_binding.zsh
+++ key_binding.zsh
@@ -1,12 +1,17 @@
-bindkey "${key[home]}"         beginning-of-line
-bindkey "${key[end]}"          end-of-line
+if [[ ! -z "$ITERM_PROFILE" ]]; then
+  key[home] = '\033[A'
+  key[end]  = '\033[B'
+fi
 
-bindkey "${key[delete]}"       delete-char
-bindkey "${key[backspace]}"    backward-delete-char
+bindkey $key[home]         beginning-of-line
+bindkey $key[end]          end-of-line
 
-bindkey "${key[up]}"           up-line-or-history
-bindkey "${key[down]}"         down-line-or-history
+bindkey $key[delete]       delete-char
+bindkey $key[backspace]    backward-delete-char
 
-bindkey "${key[ctrl-right]}"   forward-word
-bindkey "${key[ctrl-left]}"    backward-word
+bindkey $key[up]           up-line-or-history
+bindkey $key[down]         down-line-or-history
+
+bindkey $key[ctrl-right]   forward-word
+bindkey $key[ctrl-left]    backward-word
 
